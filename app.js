2e54760148fa7e7851e7c9b5457216c62042dedf
Vishesh Dadhich <visheshdadhich20@gmail.com>
2024-04-12 23:29:55 +0530

    Minor DB URL change

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/yelp-camp'
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/yelp-camp'
 
 // mongodb://127.0.0.1:27017/yelp-camp
 mongoose.connect(dbUrl).then(() => {
